4f33ecfc2d2d30466b6d37aadc3c012cb6224f67
Petter Holmstroem <petter@vaadin.com>
2013-10-10 10:06:49 +0300

    Added documentation and logging to Services

--- mockapp-server/src/main/java/com/vaadin/mockapp/backend/Services.java
+++ mockapp-server/src/main/java/com/vaadin/mockapp/backend/Services.java
@@ -1,31 +1,55 @@
 package com.vaadin.mockapp.backend;
 
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
+ * Class for looking up services by their interfaces. Service singletons are registered with this class,
+ * e.g. by servlet context listeners upon application startup, and can then be accessed by different application components.
+ *
  * @author petter@vaadin.com
  */
 public class Services {
 
-    private static final Map<Class<?>, Object> serviceMap = new HashMap<Class<?>, Object>();
+    private static final Map<Class<?>, Object> serviceMap = Collections.synchronizedMap(new HashMap<Class<?>, Object>());
 
     private Services() {
     }
 
-    public synchronized static <T> T get(Class<T> serviceClass) {
-        return serviceClass.cast(serviceMap.get(serviceClass));
+    /**
+     * Returns an instance of the specified service interface, or {@code null} if no service has been registered.
+     */
+    @Nullable
+    public synchronized static <T> T get(@Nonnull Class<T> serviceInterface) {
+        T service = serviceInterface.cast(serviceMap.get(serviceInterface));
+        if (service == null) {
+            Logger.getLogger(Services.class.getCanonicalName()).log(Level.WARNING, "No service instance found for {0}", serviceInterface);
+        }
+        return service;
     }
 
-    public synchronized static <T> void add(T service, Class<? super T>... serviceClasses) {
-        for (Class<?> serviceClass: serviceClasses) {
-            serviceMap.put(serviceClass, service);
+    /**
+     * Registers the specified service instance for the specified service interfaces.
+     */
+    public static <T> void register(@Nonnull T service, @Nonnull Class<? super T>... serviceInterfaces) {
+        for (Class<?> serviceInterface : serviceInterfaces) {
+            serviceMap.put(serviceInterface, service);
+            Logger.getLogger(Services.class.getCanonicalName()).log(Level.INFO, "Registered service instance {0} for {1}", new Object[]{service, serviceInterface});
         }
     }
 
-    public synchronized static void remove(Class<?>... serviceClasses) {
-        for (Class<?> serviceClass : serviceClasses) {
-            serviceMap.remove(serviceClass);
+    /**
+     * Removes previously registered service instances for the specified service interfaces.
+     */
+    public static void remove(@Nonnull Class<?>... serviceInterfaces) {
+        for (Class<?> serviceInterface : serviceInterfaces) {
+            serviceMap.remove(serviceInterface);
+            Logger.getLogger(Services.class.getCanonicalName()).log(Level.INFO, "Removed service registration for {0}", serviceInterface);
         }
     }
 

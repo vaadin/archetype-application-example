9ec6747ebeb01295a6b3d0b6c3193face765a5a9
Petter Holmstroem <petter@vaadin.com>
2013-10-10 09:30:35 +0300

    Added authentication classes

---
+++ mockapp-server/src/main/java/com/vaadin/mockapp/backend/authentication/Authentication.java
@@ -0,0 +1,49 @@
+package com.vaadin.mockapp.backend.authentication;
+
+import java.io.Serializable;
+
+/**
+ * Interface representing an authentication token that identifies a user and contains information about his or her permissions.
+ * The current authentication token can always be accessed by calling {@link com.vaadin.mockapp.backend.authentication.AuthenticationHolder#getAuthentication()} .
+ *
+ * @author petter@vaadin.com
+ */
+public interface Authentication extends Serializable {
+
+    /**
+     * {@code Authentication} object that indicates that the user is not authenticated. This is basically the Null Pattern
+     * but with a different name.
+     */
+    Authentication ANONYMOUS = new Authentication() {
+
+        @Override
+        public String getName() {
+            return "Anonymous";
+        }
+
+        @Override
+        public boolean hasRole(String roleName) {
+            return false;
+        }
+
+        @Override
+        public int hashCode() {
+            return 0;
+        }
+
+        @Override
+        public boolean equals(Object obj) {
+            return obj == this;
+        }
+    };
+
+    /**
+     * Returns the name of the authenticated user (never {@code null}).
+     */
+    String getName();
+
+    /**
+     * Returns whether the authenticated user holds the specified role.
+     */
+    boolean hasRole(String roleName);
+}
---
+++ mockapp-server/src/main/java/com/vaadin/mockapp/backend/authentication/AuthenticationHolder.java
@@ -0,0 +1,59 @@
+package com.vaadin.mockapp.backend.authentication;
+
+import com.vaadin.server.VaadinRequest;
+import com.vaadin.server.VaadinService;
+
+/**
+ * Class for retrieving and setting the {@link Authentication} object of the current session. All methods of this class
+ * require that a {@link VaadinRequest} is bound to the current thread.
+ *
+ * @see com.vaadin.server.VaadinService#getCurrentRequest()
+ *
+ * @author petter@vaadin.com
+ */
+public final class AuthenticationHolder {
+
+    /**
+     * The attribute key used to store the authentication token in the session.
+     */
+    public static final String AUTHENTICATION_SESSION_ATTRIBUTE_KEY = Authentication.class.getCanonicalName();
+
+    private AuthenticationHolder() {
+    }
+
+    /**
+     * Returns the authentication object stored in the current session, or {@link Authentication#ANONYMOUS} if no authentication is stored.
+     *
+     * @throws IllegalStateException if the current session cannot be accessed.
+     */
+    public static Authentication getAuthentication() {
+        Authentication authentication = (Authentication) getCurrentRequest().getWrappedSession().getAttribute(AUTHENTICATION_SESSION_ATTRIBUTE_KEY);
+        if (authentication == null) {
+            return Authentication.ANONYMOUS;
+        } else {
+            return authentication;
+        }
+    }
+
+    /**
+     * Sets the authentication object to be stored in the current session. Using a {@code null} authentication will remove
+     * the existing authentication object from the session.
+     *
+     * @throws IllegalStateException if the current session cannot be accessed.
+     */
+    public static void setAuthentication(Authentication authentication) {
+        if (authentication == null) {
+            getCurrentRequest().getWrappedSession().removeAttribute(AUTHENTICATION_SESSION_ATTRIBUTE_KEY);
+        } else {
+            getCurrentRequest().getWrappedSession().setAttribute(AUTHENTICATION_SESSION_ATTRIBUTE_KEY, authentication);
+        }
+    }
+
+    private static VaadinRequest getCurrentRequest() {
+        VaadinRequest request = VaadinService.getCurrentRequest();
+        if (request == null) {
+            throw new IllegalStateException("No request bound to current thread");
+        }
+        return request;
+    }
+}

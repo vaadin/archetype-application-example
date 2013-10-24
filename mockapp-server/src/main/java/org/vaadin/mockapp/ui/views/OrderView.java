db451b3ebe0b3252d26cae3cdc0391c3156011fe
Petter Holmstroem <petter@vaadin.com>
2013-10-24 15:52:26 +0300

    Added example of third party component, extracted OrderItemsField to its own top class

--- mockapp-server/src/main/java/org/vaadin/mockapp/ui/views/OrderView.java
+++ mockapp-server/src/main/java/org/vaadin/mockapp/ui/views/OrderView.java
@@ -1,30 +1,31 @@
 package org.vaadin.mockapp.ui.views;
 
+import com.vaadin.data.fieldgroup.BeanFieldGroup;
 import com.vaadin.data.fieldgroup.PropertyId;
-import com.vaadin.data.util.BeanItemContainer;
 import com.vaadin.data.validator.EmailValidator;
 import com.vaadin.event.FieldEvents;
 import com.vaadin.navigator.View;
 import com.vaadin.navigator.ViewChangeListener;
 import com.vaadin.ui.*;
+import fi.jasoft.qrcode.QRCode;
 import org.vaadin.mockapp.ResetButtonForTextField;
+import org.vaadin.mockapp.Services;
 import org.vaadin.mockapp.backend.MockAppRoles;
 import org.vaadin.mockapp.backend.domain.ContactMethod;
-import org.vaadin.mockapp.backend.domain.OrderItem;
+import org.vaadin.mockapp.backend.domain.Order;
 import org.vaadin.mockapp.backend.domain.OrderState;
+import org.vaadin.mockapp.backend.services.OrderService;
 import org.vaadin.mockapp.ui.theme.MockAppTheme;
-import org.vaadin.mockapp.ui.views.components.DateTimeField;
-import org.vaadin.mockapp.ui.views.components.H1;
-import org.vaadin.mockapp.ui.views.components.H2;
+import org.vaadin.mockapp.ui.views.components.*;
 
-import java.util.List;
+import java.util.UUID;
 
 /**
  * @author petter@vaadin.com
  */
 @ViewDefinition(name = OrderView.VIEW_NAME,
         allowedRoles = {MockAppRoles.ROLE_SALESMAN, MockAppRoles.ROLE_OBSERVER})
-public class OrderView extends VerticalLayout implements View, FieldEvents.TextChangeListener {
+public class OrderView extends VerticalLayout implements View, FieldEvents.TextChangeListener, OrderItemsField.SelectionCallback {
 
     public static final String VIEW_NAME = "order";
     private H1 title;
@@ -39,7 +40,7 @@ public class OrderView extends VerticalLayout implements View, FieldEvents.TextC
     @PropertyId("customerName")
     private TextField customerName;
     @PropertyId("customerAddress.street")
-    private TextField shippingAddress;
+    private TextField shippingStreet;
     @PropertyId("customerAddress.postalCode")
     private TextField shippingPostalCode;
     @PropertyId("customerAddress.city")
@@ -54,17 +55,19 @@ public class OrderView extends VerticalLayout implements View, FieldEvents.TextC
     private TextField billingPostalCode;
     @PropertyId("billingAddress.city")
     private TextField billingCity;
-    private CheckBox billingAddressSameAsCustomer;
+    private Button copyFromShippingAddress;
     @PropertyId("state")
     private ComboBox state;
     @PropertyId("items")
     private OrderItemsField items;
+    private QRCode qrCode;
     private TextField filterItems;
     private Button save;
     private Button backWithoutSaving;
     private Button addItem;
     private Button editItem;
     private Button deleteItem;
+    private BeanFieldGroup<Order> binder;
 
     public OrderView() {
         init();
@@ -75,12 +78,20 @@ public class OrderView extends VerticalLayout implements View, FieldEvents.TextC
         setMargin(true);
         setSpacing(true);
 
-        addComponent(title = new H1("Order"));
-
         HorizontalLayout group;
         addComponent(group = createGroup());
+        group.setWidth("100%");
+        group.addComponent(title = new H1("Order Details"));
+
         group.addComponent(orderNo = new TextField("Order No"));
-        group.addComponent(state = new ComboBox("Order State"));
+        group.setComponentAlignment(orderNo, Alignment.MIDDLE_RIGHT);
+        group.setExpandRatio(orderNo, 1);
+        orderNo.setWidth("60px");
+        orderNo.setConverter(new OrderNoConverter());
+
+        group.addComponent(state = new ComboBox("State"));
+        group.setComponentAlignment(state, Alignment.MIDDLE_RIGHT);
+        state.setWidth("140px");
         state.setTextInputAllowed(false);
         state.setNullSelectionAllowed(false);
         state.setRequired(true);
@@ -89,12 +100,13 @@ public class OrderView extends VerticalLayout implements View, FieldEvents.TextC
             state.setItemCaption(s, s.getDisplayName());
         }
 
-        addComponent(new H2("Order Reception"));
-        addComponent(orderReceived = new DateTimeField("Date & time"));
-        orderReceived.setRequired(true);
-
         addComponent(group = createGroup());
-        group.addComponent(orderReceivedVia = new ComboBox("Method"));
+
+        group.addComponent(orderReceived = new DateTimeField("Order received on"));
+        orderReceived.setRequired(true);
+        orderReceived.setDateFormat("d.M.yy");
+
+        group.addComponent(orderReceivedVia = new ComboBox("Order received via"));
         orderReceivedVia.setTextInputAllowed(false);
         orderReceivedVia.setNullSelectionAllowed(false);
         orderReceivedVia.setRequired(true);
@@ -102,44 +114,90 @@ public class OrderView extends VerticalLayout implements View, FieldEvents.TextC
             orderReceivedVia.addItem(method);
             orderReceivedVia.setItemCaption(method, method.getDisplayName());
         }
-        orderReceivedVia.setWidth(8, Unit.EM);
+        orderReceivedVia.setWidth("130px");
 
-        group.addComponent(orderReceivedBy = new TextField("Employee who received the order"));
+        group.addComponent(orderReceivedBy = new TextField("Order received by"));
         orderReceivedBy.setRequired(true);
-        orderReceivedBy.setWidth(20, Unit.EM);
+        orderReceivedBy.setWidth("150px");
 
-        addComponent(new H2("Customer Details"));
-        addComponent(customerName = new TextField("Customer Name"));
-        customerName.setWidth(20, Unit.EM);
-
+        addComponent(new H2("Customer"));
         addComponent(group = createGroup());
-        group.setCaption("Shipping Address");
-        group.addComponent(shippingAddress = new TextField());
-        shippingAddress.setWidth(20, Unit.EM);
-        group.addComponent(shippingPostalCode = new TextField());
-        shippingPostalCode.setWidth(5, Unit.EM);
-        group.addComponent(shippingCity = new TextField());
-        shippingCity.setWidth(15, Unit.EM);
 
-        addComponent(group = createGroup());
+        group.addComponent(customerName = new TextField("Customer Name"));
+        customerName.setWidth("200px");
+
         group.addComponent(customerEmail = new TextField("E-mail"));
+        customerEmail.setNullRepresentation("");
         customerEmail.addValidator(new EmailValidator("Please enter a valid e-mail address"));
-        customerEmail.setWidth(20, Unit.EM);
+        customerEmail.setWidth("200px");
+
         group.addComponent(customerPhone = new TextField("Phone"));
-        customerPhone.setWidth(10, Unit.EM);
+        customerPhone.setNullRepresentation("");
+        customerPhone.setWidth("110px");
 
-        addComponent(new H2("Billing Details"));
-        addComponent(billingAddressSameAsCustomer = new CheckBox("Billing address same as shipping address"));
 
-        addComponent(group = createGroup());
-        group.setCaption("Billing Address");
-        group.addComponent(billingStreet = new TextField());
-        billingStreet.setWidth(20, Unit.EM);
-        group.addComponent(billingPostalCode = new TextField());
-        billingPostalCode.setWidth(5, Unit.EM);
-        group.addComponent(billingCity = new TextField());
-        billingCity.setWidth(15, Unit.EM);
+        HorizontalLayout columns = new HorizontalLayout();
+        addComponent(columns);
+        columns.setSpacing(true);
+        {
+            VerticalLayout column = new VerticalLayout();
+            column.setSpacing(true);
+            columns.addComponent(column);
 
+            column.addComponent(new H2("Shipping Address"));
+
+            column.addComponent(shippingStreet = new TextField());
+            shippingStreet.setWidth("200px");
+            shippingStreet.setNullRepresentation("");
+
+            group = createGroup();
+            group.setWidth("200px");
+            column.addComponent(group);
+
+            group.addComponent(shippingPostalCode = new TextField());
+            shippingPostalCode.setWidth("60px");
+            shippingPostalCode.setNullRepresentation("");
+            group.addComponent(shippingCity = new TextField());
+            group.setExpandRatio(shippingCity, 1);
+            shippingCity.setWidth("100%");
+            shippingCity.setNullRepresentation("");
+
+        }
+        {
+            VerticalLayout column = new VerticalLayout();
+            column.setSpacing(true);
+            columns.addComponent(column);
+
+            column.addComponent(group = createGroup());
+            group.addComponent(new H2("Billing Address"));
+
+            group.addComponent(copyFromShippingAddress = new Button("Copy from shipping address"));
+            copyFromShippingAddress.addStyleName(MockAppTheme.BUTTON_LINK);
+
+            column.addComponent(billingStreet = new TextField());
+            billingStreet.setWidth("200px");
+            billingStreet.setNullRepresentation("");
+
+            group = createGroup();
+            group.setWidth("200px");
+            column.addComponent(group);
+
+            group.addComponent(billingPostalCode = new TextField());
+            billingPostalCode.setWidth("60px");
+            billingPostalCode.setNullRepresentation("");
+            group.addComponent(billingCity = new TextField());
+            group.setExpandRatio(billingCity, 1);
+            billingCity.setWidth("100%");
+            billingCity.setNullRepresentation("");
+        }
+        {
+            qrCode = new QRCode();
+            qrCode.setDescription("This does not provide any value to the application, it is just here to demo how to use a third party add-on from the directory.");
+            qrCode.setWidth("100px");
+            qrCode.setHeight("100px");
+            columns.addComponent(qrCode);
+            qrCode.setValue("https://www.vaadin.com");
+        }
 
         addComponent(group = createGroup());
         group.setWidth("100%");
@@ -152,6 +210,7 @@ public class OrderView extends VerticalLayout implements View, FieldEvents.TextC
         group.setComponentAlignment(filterItems, Alignment.MIDDLE_RIGHT);
 
         addComponent(items = new OrderItemsField());
+        items.setSelectionCallback(this);
         items.setSizeFull();
         setExpandRatio(items, 1);
 
@@ -169,6 +228,8 @@ public class OrderView extends VerticalLayout implements View, FieldEvents.TextC
 
         group.addComponent(backWithoutSaving = new Button("Go back without saving"));
         group.setComponentAlignment(backWithoutSaving, Alignment.MIDDLE_RIGHT);
+
+        binder = new BeanFieldGroup<Order>(Order.class);
     }
 
     private HorizontalLayout createGroup() {
@@ -184,60 +245,43 @@ public class OrderView extends VerticalLayout implements View, FieldEvents.TextC
 
     @Override
     public void enter(ViewChangeListener.ViewChangeEvent event) {
-        //To change body of implemented methods use File | Settings | File Templates.
+        Order order = null;
+        try {
+            UUID uuid = UUID.fromString(event.getParameters());
+            order = getOrderService().findByUuid(uuid);
+        } catch (IllegalArgumentException ex) {
+        }
+        if (order == null) {
+            order = new Order();
+        }
+        setOrder(order);
     }
 
-    public static class OrderItemsField extends CustomField<List> {
+    private void setOrder(Order order) {
+        orderNo.setReadOnly(false);
+        binder.setItemDataSource(order);
+        binder.getItemDataSource().addNestedProperty("customerAddress.street");
+        binder.getItemDataSource().addNestedProperty("customerAddress.postalCode");
+        binder.getItemDataSource().addNestedProperty("customerAddress.city");
+        binder.getItemDataSource().addNestedProperty("billingAddress.street");
+        binder.getItemDataSource().addNestedProperty("billingAddress.postalCode");
+        binder.getItemDataSource().addNestedProperty("billingAddress.city");
+        binder.bindMemberFields(this);
+        orderNo.setReadOnly(true);
+        updateButtonStates();
+    }
 
-        private BeanItemContainer<OrderItem> itemsContainer;
-        private Table itemsTable;
+    private OrderService getOrderService() {
+        return Services.get(OrderService.class);
+    }
 
-        public OrderItemsField() {
-            itemsContainer = new BeanItemContainer<OrderItem>(OrderItem.class);
-            itemsTable = new Table();
-            itemsTable.setContainerDataSource(itemsContainer);
-            itemsTable.setSelectable(true);
-            itemsTable.setImmediate(true);
-            itemsTable.setVisibleColumns("description", "qty", "unit", "unitPrice", "taxPercentage", "totalTax", "totalWithoutTax");
-            itemsTable.setColumnHeaders("Description", "Qty", "Unit", "Unit Price", "Tax-%", "Total Tax", "Total (excluding tax)");
-            itemsTable.setColumnAlignment("qty", Table.Align.RIGHT);
-            itemsTable.setColumnAlignment("unitPrice", Table.Align.RIGHT);
-            itemsTable.setColumnAlignment("taxPercentage", Table.Align.RIGHT);
-            itemsTable.setColumnAlignment("totalTax", Table.Align.RIGHT);
-            itemsTable.setColumnAlignment("totalWithoutTax", Table.Align.RIGHT);
-        }
+    private void updateButtonStates() {
+        editItem.setEnabled(items.hasSelection());
+        deleteItem.setEnabled(items.hasSelection());
+    }
 
-        public void filter(String filterString) {
-
-        }
-
-        @Override
-        @SuppressWarnings("unchecked")
-        protected void setInternalValue(List newValue) {
-            super.setInternalValue(newValue);
-            itemsContainer.addAll(newValue);
-        }
-
-        @Override
-        protected Component initContent() {
-            return itemsTable;
-        }
-
-        @Override
-        public Class<? extends List> getType() {
-            return List.class;
-        }
-
-        public void addItem() {
-
-        }
-
-        public void editSelected() {
-
-        }
-
-        public void deleteSelected() {
-
-        }
+    @Override
+    public void onSelectionChanged(OrderItemsField sender, boolean hasSelection) {
+        updateButtonStates();
     }
 }

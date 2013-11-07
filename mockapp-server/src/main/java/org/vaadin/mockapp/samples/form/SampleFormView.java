8084d71831c04632ee80a96cde3d5e52b566947a
Petter Holmstroem <petter@vaadin.com>
2013-11-07 08:51:51 +0200

    Implemented details field

--- mockapp-server/src/main/java/org/vaadin/mockapp/samples/form/SampleFormView.java
+++ mockapp-server/src/main/java/org/vaadin/mockapp/samples/form/SampleFormView.java
@@ -1,6 +1,7 @@
 package org.vaadin.mockapp.samples.form;
 
 import com.vaadin.data.fieldgroup.BeanFieldGroup;
+import com.vaadin.data.fieldgroup.FieldGroup;
 import com.vaadin.data.fieldgroup.PropertyId;
 import com.vaadin.data.util.BeanItem;
 import com.vaadin.data.validator.IntegerRangeValidator;
@@ -32,6 +33,10 @@ public class SampleFormView extends VerticalLayout implements View {
     private CheckBox booleanField;
     @PropertyId("embeddedProperty.enumProperty")
     private ComboBox enumField;
+    @PropertyId("details")
+    private SampleDetailsField detailsField;
+    private Button commit;
+    private Button discard;
     private BeanFieldGroup<SampleMaster> binder;
 
     public SampleFormView() {
@@ -43,15 +48,41 @@ public class SampleFormView extends VerticalLayout implements View {
         uuid.setConverter(new UUIDToStringConverter());
         formLayout.addComponent(stringField = new TextField("String"));
         stringField.addValidator(new StringLengthValidator("The string must be between 10 and 30 characters", 10, 30, false));
+        stringField.setImmediate(true);
         formLayout.addComponent(integerField = new TextField("Integer"));
         integerField.addValidator(new IntegerRangeValidator("The integer must be positive", 0, Integer.MAX_VALUE));
+        integerField.setImmediate(true);
         formLayout.addComponent(bigDecimalField = new TextField("BigDecimal"));
         bigDecimalField.setConverter(new BigDecimalToStringConverter());
+        bigDecimalField.setImmediate(true);
         formLayout.addComponent(booleanField = new CheckBox("Boolean"));
         formLayout.addComponent(enumField = new ComboBox("Enum"));
         for (SampleEnum sampleEnum : SampleEnum.values()) {
             enumField.addItem(sampleEnum);
         }
+        formLayout.addComponent(detailsField = new SampleDetailsField());
+        detailsField.setHeight("300px");
+
+        commit = new Button("Commit", new Button.ClickListener() {
+            @Override
+            public void buttonClick(Button.ClickEvent event) {
+                try {
+                    binder.commit();
+                    // TODO Save the bean to some backend
+                } catch (FieldGroup.CommitException ex) {
+                    // Let the binder handle it
+                }
+            }
+        });
+        discard = new Button("Discard", new Button.ClickListener() {
+            @Override
+            public void buttonClick(Button.ClickEvent event) {
+                binder.discard();
+            }
+        });
+        HorizontalLayout buttons = new HorizontalLayout(commit, discard);
+        buttons.setSpacing(true);
+        formLayout.addComponent(buttons);
 
         binder = new BeanFieldGroup<SampleMaster>(SampleMaster.class);
         binder.bindMemberFields(this);

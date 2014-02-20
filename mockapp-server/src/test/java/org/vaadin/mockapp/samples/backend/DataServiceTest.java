ffeec7f18a701a911b703c3bb49a302422a10f82
Jonatan Kronqvist <jonatan@vaadin.com>
2014-02-20 08:26:52 +0200

    Added a couple of really simple backend tests

---
+++ mockapp-server/src/test/java/org/vaadin/mockapp/samples/backend/DataServiceTest.java
@@ -0,0 +1,37 @@
+package org.vaadin.mockapp.samples.backend;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.vaadin.mockapp.samples.data.Product;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+
+public class DataServiceTest {
+
+    private DataService service;
+
+    @Before
+    public void setUp() throws Exception {
+        service = MockDataService.getInstance();
+    }
+
+    @Test
+    public void testDataServiceCanFetchProducts() throws Exception {
+        assertFalse(service.getAllProducts().isEmpty());
+    }
+
+    @Test
+    public void testDataServiceCanFetchCategories() throws Exception {
+        assertFalse(service.getAllCategories().isEmpty());
+    }
+
+    @Test
+    public void testUpdateProduct_updatesTheProduct() throws Exception {
+        Product p = service.getAllProducts().iterator().next();
+        p.setProductName("My Test Name");
+        service.updateProduct(p);
+        Product p2 = service.getAllProducts().iterator().next();
+        assertEquals("My Test Name", p2.getProductName());
+    }
+}

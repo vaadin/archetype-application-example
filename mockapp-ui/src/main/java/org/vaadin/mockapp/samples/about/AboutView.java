0cf56b9f9eb4a927064671825cef232f43abdeef
Henri Sara <hesara@vaadin.com>
2014-11-25 09:49:25 +0200

    Some about view adjustments

--- mockapp-ui/src/main/java/org/vaadin/mockapp/samples/about/AboutView.java
+++ mockapp-ui/src/main/java/org/vaadin/mockapp/samples/about/AboutView.java
@@ -16,6 +16,10 @@ public class AboutView extends VerticalLayout implements View {
 
 	public static final String VIEW_NAME = "About";
 	
+	public static final String ABOUT_TEXT = "<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce porta, risus sit amet imperdiet ornare, eros nunc faucibus nulla, facilisis varius ante enim ut tellus. Donec ac suscipit enim.</p>"
+			+ "<p>Cras luctus eget dui viverra vehicula. Curabitur molestie aliquet mi, eget pellentesque dolor lacinia ut. Donec molestie mi ac neque porttitor, eu pretium augue vehicula. Praesent vel tellus blandit enim tempor aliquet.</p>"
+			+ "<p>Duis sit amet tempor justo. Vestibulum ac lacus nisl. Curabitur lacinia dui eget tortor molestie, vel euismod dolor consectetur. Aliquam viverra augue in vulputate accumsan.</p>";
+	
 	public AboutView() {
 		CssLayout aboutContent = new CssLayout();
 		aboutContent.setStyleName("about-content");
@@ -23,7 +27,7 @@ public class AboutView extends VerticalLayout implements View {
 		// this could also be a single long HTML Label
 		aboutContent.addComponent(new Label("<h1>About</h1>", ContentMode.HTML));
 		aboutContent.addComponent(new Label(FontAwesome.INFO_CIRCLE.getHtml() + "Text area width should not be over 500px", ContentMode.HTML));
-		aboutContent.addComponent(new Label("Lorem ipsum ..."));
+		aboutContent.addComponent(new Label(ABOUT_TEXT, ContentMode.HTML));
 		aboutContent.addComponent(new Link("More information", new ExternalResource("http://vaadin.com/")));
 		
 		setSizeFull();

39fd837b1f8517126b85b27463168037ed9f8518
Artur Signell <artur@vaadin.com>
2014-11-27 19:38:18 +0200

    Improved about text

--- mockapp-ui/src/main/java/org/vaadin/mockapp/samples/about/AboutView.java
+++ mockapp-ui/src/main/java/org/vaadin/mockapp/samples/about/AboutView.java
@@ -3,6 +3,7 @@ package org.vaadin.mockapp.samples.about;
 import com.vaadin.navigator.View;
 import com.vaadin.navigator.ViewChangeListener.ViewChangeEvent;
 import com.vaadin.server.FontAwesome;
+import com.vaadin.shared.Version;
 import com.vaadin.shared.ui.label.ContentMode;
 import com.vaadin.ui.Alignment;
 import com.vaadin.ui.CustomLayout;
@@ -18,7 +19,7 @@ public class AboutView extends VerticalLayout implements View {
 		aboutContent.setStyleName("about-content");
 
 		// you can add Vaadin components in predefined slots in the custom layout
-		aboutContent.addComponent(new Label(FontAwesome.INFO_CIRCLE.getHtml() + "Text area width should not be over 500px", ContentMode.HTML), "info");
+		aboutContent.addComponent(new Label(FontAwesome.INFO_CIRCLE.getHtml() + " This application is using Vaadin "+Version.getFullVersion(), ContentMode.HTML), "info");
 		
 		setSizeFull();
 		setStyleName("about-view");

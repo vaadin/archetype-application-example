9b75ae9acde7ca284ed9301be8eead9b2995652b
Artur Signell <artur@vaadin.com>
2015-02-03 09:54:44 +0200

    Use current session instead of current request
    In a background thread, the session is available but no request
    
    Change-Id: I63f213a8b40eb025265c4d46db16f2843963105c

--- mockapp-ui/src/main/java/org/vaadin/mockapp/samples/authentication/CurrentUser.java
+++ mockapp-ui/src/main/java/org/vaadin/mockapp/samples/authentication/CurrentUser.java
@@ -1,14 +1,15 @@
 package org.vaadin.mockapp.samples.authentication;
 
 import com.vaadin.server.VaadinRequest;
-import com.vaadin.server.VaadinService;
+import com.vaadin.server.VaadinSession;
+import com.vaadin.server.WrappedSession;
 
 /**
  * Class for retrieving and setting the name of the current user of the current
  * session (without using JAAS). All methods of this class require that a
  * {@link VaadinRequest} is bound to the current thread.
- * 
- * 
+ *
+ *
  * @see com.vaadin.server.VaadinService#getCurrentRequest()
  */
 public final class CurrentUser {
@@ -25,13 +26,13 @@ public final class CurrentUser {
     /**
      * Returns the name of the current user stored in the current session, or an
      * empty string if no user name is stored.
-     * 
+     *
      * @throws IllegalStateException
      *             if the current session cannot be accessed.
      */
     public static String get() {
-        String currentUser = (String) getCurrentRequest().getWrappedSession()
-                .getAttribute(CURRENT_USER_SESSION_ATTRIBUTE_KEY);
+        String currentUser = (String) getCurrentHttpSession().getAttribute(
+                CURRENT_USER_SESSION_ATTRIBUTE_KEY);
         if (currentUser == null) {
             return "";
         } else {
@@ -39,29 +40,30 @@ public final class CurrentUser {
         }
     }
 
+    private static WrappedSession getCurrentHttpSession() {
+        VaadinSession s = VaadinSession.getCurrent();
+        if (s == null) {
+            throw new IllegalStateException(
+                    "No session found for current thread");
+        }
+        return s.getSession();
+    }
+
     /**
      * Sets the name of the current user and stores it in the current session.
      * Using a {@code null} username will remove the username from the session.
-     * 
+     *
      * @throws IllegalStateException
      *             if the current session cannot be accessed.
      */
     public static void set(String currentUser) {
         if (currentUser == null) {
-            getCurrentRequest().getWrappedSession().removeAttribute(
+            getCurrentHttpSession().removeAttribute(
                     CURRENT_USER_SESSION_ATTRIBUTE_KEY);
         } else {
-            getCurrentRequest().getWrappedSession().setAttribute(
+            getCurrentHttpSession().setAttribute(
                     CURRENT_USER_SESSION_ATTRIBUTE_KEY, currentUser);
         }
     }
 
-    private static VaadinRequest getCurrentRequest() {
-        VaadinRequest request = VaadinService.getCurrentRequest();
-        if (request == null) {
-            throw new IllegalStateException(
-                    "No request bound to current thread");
-        }
-        return request;
-    }
 }
